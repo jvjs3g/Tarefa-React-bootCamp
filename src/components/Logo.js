7f9dd1c483535faed5dca581528ee433c7606c7c
joao vitor <joao3klb@hotmail.com>
2020-06-04 21:22:58 -0300

    React

---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import React, { Component}from 'react';
+import './Logo.css'
+class Logo extends Component{
+  render(){
+    return(
+      <form>
+       <div className="logo">
+         Facebook
+       </div>
+      </form>
+    );
+  }
+}
+export default Logo;
